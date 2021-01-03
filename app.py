cb75da46f24c310d832f3ae17a47d3edc7f3eb29
jaden <806174970@qq.com>
2021-01-03 20:27:37 +0800

    auth

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask
-
+from View import auth
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+app.register_blueprint(auth.bp_auth)
 
 if __name__ == '__main__':
     app.run()
