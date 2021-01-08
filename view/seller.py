9494966936510da3eea6ed2f049d8e78a2ff393d
HelloWorld-404 <68452137+HelloWorld-404@users.noreply.github.com>
2021-01-08 19:11:13 +0800

    Add files via upload

--- view/seller.py
+++ view/seller.py
@@ -1,16 +1,18 @@
 from flask import Blueprint
 from flask import request
 from flask import jsonify
-from model import seller as se
+from be.model import seller
 import json
+
 bp_seller = Blueprint("seller", __name__, url_prefix="/seller")
 
 
 @bp_seller.route("/create_store", methods=["POST"])
 def seller_create_store():
-    user_id: str = request.json.get("user_id")
-    store_id: str = request.json.get("store_id")
-    code, message = se.create_store(user_id, store_id)
+    user_id: str = request.json.get("user_id", "")
+    store_id: str = request.json.get("store_id", "")
+    se = seller.Seller()
+    code, message = se.create_store(user_id=user_id, store_id=store_id)
     return jsonify({"message": message}), code
 
 
@@ -20,8 +22,9 @@ def seller_add_book():
     store_id: str = request.json.get("store_id")
     book_info: str = request.json.get("book_info")
     stock_level: str = request.json.get("stock_level", 0)
-    code, message = se.add_book(user_id, store_id, json.loads(book_info).get("id"), json.dumps(book_info),
-                             eval(stock_level))
+    se = seller.Seller()
+    code, message = se.add_book(user_id, store_id, book_info.get("id"), json.dumps(book_info),
+                                int(stock_level))
     return jsonify({"message": message}), code
 
 
@@ -31,5 +34,6 @@ def add_stock_level():
     store_id: str = request.json.get("store_id")
     book_id: str = request.json.get("book_id")
     add_num: str = request.json.get("add_stock_level", 0)
-    code, message = se.add_stock_level(user_id, store_id, book_id, eval(add_num))
+    se = seller.Seller()
+    code, message = se.add_stock_level(user_id, store_id, book_id, int(add_num))
     return jsonify({"message": message}), code
