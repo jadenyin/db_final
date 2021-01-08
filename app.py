398124190436abe645b397feac1c0b111a315e6a
jaden <806174970@qq.com>
2021-01-08 13:34:52 +0800

    merge

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
-from View import auth
+from view import auth
+from view import seller
 app = Flask(__name__)
 
 app.register_blueprint(auth.bp_auth)
+app.register_blueprint(seller.bp_seller)
 
 if __name__ == '__main__':
     app.run()
